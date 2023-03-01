9e42883258a6047a9dd7c4e2f2029e68f3f9b8ae
Kieran Davis <kieran.davis123@protonmail.com>
2023-03-01 19:32:57 +0000

    Feat: Many Things

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from "next/head";
-import Footer from "@/Components/Footer";
+import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -10,8 +11,8 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
-      <main className="bg-main h-screen w-full">
+      {/* Navbar */}
+      <main className="bg-main h- w-full">
         <div className="drawer">
           <input id="my-drawer-3" type="checkbox" className="drawer-toggle" />
           <div className="drawer-content flex flex-col">
@@ -36,24 +37,32 @@ export default function Home() {
                   </svg>
                 </label>
               </div>
-              <div className="flex-1 px-2 mx-2 text-4xl">Elker</div>
+              <div className="flex-1 px-2 mx-2 text-4xl">
+                <Link href="/">Elker</Link>
+              </div>
               <div className="flex-none hidden lg:block text-2xl">
                 <ul className="menu menu-horizontal">
                   <li>
                     <a className="rounded-md">About Me</a>
                   </li>
                   <li>
-                    <a className="rounded-md">Projects</a>
+                    <Link href="/projects#Jellyfin" className="rounded-md">
+                      Projects
+                    </Link>
                   </li>
                 </ul>
               </div>
             </div>
-            <div id="CONTENT">
+            {/* Page Content */}
+            <div>
               <div className="hero min-h-screen">
                 <div className="hero-content flex-col lg:flex-row">
-                  <img
-                    src="https://s.elker54.com/RIZo8/jirEJOKE34/raw.png"
+                  <Image
+                    src="/PFP-Small.png"
                     className="md:max-w-sm rounded-full border-4"
+                    width={400}
+                    height={400}
+                    alt="Profile Picture"
                   />
                   <div>
                     <div className="md:chat-start">
@@ -61,9 +70,10 @@ export default function Home() {
                         Hey! I'm Elker
                       </h1>
                     </div>
-                    <p className="py-3 text-3xl text-">
+                    <p className="text-3xl text-">
                       Take a look around my website.
                     </p>
+                    <p className=" text-2xl text-">3rd times the charm! :)</p>
 
                     <div className="flex gap-2">
                       <a href="https://elker.cc/gh" target="_blank">
@@ -112,12 +122,6 @@ export default function Home() {
                     </a>
                   </div>
                   <div className="grid grid-flow-col gap-4">
-                    <a>
-                      <i className="fa-brands fa-discord text-2xl"></i>
-                    </a>
-                    <a>
-                      <i className="fa-brands fa-github text-2xl"></i>
-                    </a>
                     <div>
                       <p>Copyright © 2023 - Some rights reserved</p>
                     </div>
@@ -126,6 +130,7 @@ export default function Home() {
               </footer>
             </div>
           </div>
+          {/* Mobile Menu */}
           <div className="drawer-side">
             <label htmlFor="my-drawer-3" className="drawer-overlay"></label>
             <ul className="menu p-4 w-80 border-r bg-base-100/80">
@@ -133,7 +138,7 @@ export default function Home() {
                 <a>About Me</a>
               </li>
               <li>
-                <a>Projects</a>
+                <a href="/projects#Jellyfin">Projects</a>
               </li>
             </ul>
           </div>
---
+++ pages/projects/index.js
@@ -0,0 +1,235 @@
+import Head from "next/head";
+import Image from "next/image";
+import Link from "next/link";
+
+function myFunction() {
+  navigator.clipboard.writeText("https://elker54.com/projects");
+}
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Elker</title>
+        <meta name="description" content="Hey, I'm Elker" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      {/* Navbar */}
+      <main className="bg-main h-screen w-full">
+        <div className="drawer">
+          <input id="my-drawer-3" type="checkbox" className="drawer-toggle" />
+          <div className="drawer-content flex flex-col">
+            <div className="w-full navbar border-b bg-base-100/70 ">
+              <div className="flex-none lg:hidden">
+                <label
+                  htmlFor="my-drawer-3"
+                  className="btn btn-square btn-ghost"
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    className="inline-block w-6 h-6 stroke-current"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="M4 6h16M4 12h16M4 18h16"
+                    ></path>
+                  </svg>
+                </label>
+              </div>
+              <div className="flex-1 px-2 mx-2 text-4xl">
+                <Link href="/">Elker</Link>
+              </div>
+              <div className="flex-none hidden lg:block text-2xl">
+                <ul className="menu menu-horizontal">
+                  <li>
+                    <a className="rounded-md">About Me</a>
+                  </li>
+                  <li>
+                    <Link href="/projects#Jellyfin" className="rounded-md">
+                      Projects
+                    </Link>
+                  </li>
+                </ul>
+              </div>
+            </div>
+            {/* Page Content */}
+            <div className="hero min-h-screen">
+              <h1>Here are some of my projects</h1>
+              <div className="carousel carousel-center p-4 space-x-4 scrollbar-thin bg-neutral rounded-box max-w-screen-xl border-4 border-emerald-500">
+                {/* Proxmox */}
+                <div className="carousel-item min-h-32" id="Proxmox">
+                  <div className="card w-96 bg-base-100 shadow-xl">
+                    <figure className="px-10 pt-10">
+                      <img
+                        src="Proxmox.png"
+                        alt="Shoes"
+                        className="rounded-xl"
+                      />
+                    </figure>
+                    <div className="card-body items-center text-center">
+                      <h2 className="card-title">Proxmox</h2>
+                      <p>
+                        Open Source Hyperviser <br /> that just works? <br />
+                        Yes please!
+                      </p>
+                      <button className="btn btn-primary">More Info</button>
+                    </div>
+                  </div>
+                </div>
+                {/* TrueNAS */}
+                <div className="carousel-item" id="TrueNAS">
+                  <div className="card w-96 bg-base-100 shadow-xl">
+                    <figure className="px-10 pt-10">
+                      <img
+                        src="TrueNAS.png"
+                        alt="Shoes"
+                        className="rounded-xl"
+                      />
+                    </figure>
+                    <div className="card-body items-center text-center">
+                      <h2 className="card-title">TrueNAS</h2>
+                      <p>
+                        Simple, but customisable <br /> storage sever for Free?
+                        <br /> Why not?
+                      </p>
+                      <div className="card-actions">
+                        <button className="btn btn-primary">More Info</button>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                {/* Jellyfin */}
+                <div className="carousel-item" id="Jellyfin">
+                  <div className="card w-96 bg-base-100 shadow-xl">
+                    <figure className="px-10 pt-10">
+                      <img
+                        src="Jellyfin.png"
+                        alt="Shoes"
+                        className="rounded-xl"
+                      />
+                    </figure>
+                    <div className="card-body items-center text-center">
+                      <h2 className="card-title">Jellyfin</h2>
+                      <p>
+                        I didn't know you could watch <br />
+                        linux ISOs? <br /> All totally legal!
+                      </p>
+                      <div className="card-actions">
+                        <button className="btn btn-primary">More Info</button>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                {/* Uptime Kuma */}
+                <div className="carousel-item" id="Uptime">
+                  <div className="card w-96 bg-base-100 shadow-xl">
+                    <figure className="px-10 pt-10">
+                      <img
+                        src="Uptime Kuma.png"
+                        alt="Shoes"
+                        className="rounded-xl"
+                      />
+                    </figure>
+                    <div className="card-body items-center text-center">
+                      <h2 className="card-title">Uptime Kuma</h2>
+                      <p>
+                        CAN YOU HEAR ME? <br />
+                        .......
+                        <br /> YES I CAN HEAR YOU
+                      </p>
+                      <div className="card-actions">
+                        <button className="btn btn-primary">More Info</button>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                {/* Carousel Item 5 */}
+                <div className="carousel-item" id="slide5">
+                  <div className="card w-96 bg-base-100 shadow-xl">
+                    <figure className="px-10 pt-10">
+                      <img
+                        src="PFP-Square.png"
+                        alt="Shoes"
+                        className="rounded-xl"
+                      />
+                    </figure>
+                    <div className="card-body items-center text-center">
+                      <h2 className="card-title">Shoes!</h2>
+                      <p>5. If a dog chews shoes whose shoes does he choose?</p>
+                      <div className="card-actions">
+                        <button className="btn btn-primary">Buy Now</button>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                {/* Carousel Item 6 */}
+                <div className="carousel-item" id="slide6">
+                  <div className="card w-96 bg-base-100 shadow-xl">
+                    <figure className="px-10 pt-10">
+                      <img
+                        src="PFP-Square.png"
+                        alt="Shoes"
+                        className="rounded-xl"
+                      />
+                    </figure>
+                    <div className="card-body items-center text-center">
+                      <h2 className="card-title">Shoes!</h2>
+                      <p>6. If a dog chews shoes whose shoes does he choose?</p>
+                      <div className="card-actions">
+                        <button className="btn btn-primary">Buy Now</button>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <footer
+              id="Footer"
+              className="footer footer-center py-2 bg-base-200/50 text-base-content border-t"
+            >
+              <div>
+                <div className="grid grid-flow-col gap-4">
+                  <a href="" className="link link-hover"></a>
+                  <a
+                    href="https://elker.cc/webemail"
+                    className="link link-hover"
+                  >
+                    Contact Me
+                  </a>
+                  <a href="https://elker.cc/hrzn" className="link link-hover">
+                    HRZN
+                  </a>
+                  <a href="https://" className="link link-hover">
+                    Code
+                  </a>
+                </div>
+                <div className="grid grid-flow-col gap-4">
+                  <div>
+                    <p>Copyright © 2023 - Some rights reserved</p>
+                  </div>
+                </div>
+              </div>
+            </footer>
+          </div>
+          {/* Mobile Menu */}
+          <div className="drawer-side">
+            <label htmlFor="my-drawer-3" className="drawer-overlay"></label>
+            <ul className="menu p-4 w-80 border-r bg-base-100/80">
+              <li>
+                <a>About Me</a>
+              </li>
+              <li>
+                <a href="/projects#Jellyfin">Projects</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </main>
+    </>
+  );
+}
